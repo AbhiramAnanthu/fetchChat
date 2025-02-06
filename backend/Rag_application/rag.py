52155a81153388c651a6d0b8087ead65f32603fa
AbhiramAnanthu <abhiramajithr@gmail.com>
2025-02-06 23:49:49 +0530

    rag rewrite, some evaluation, and added a filtering for url in strea_response

--- backend/Rag_application/rag.py
+++ backend/Rag_application/rag.py
@@ -8,76 +8,96 @@ from llama_index.readers.web import SimpleWebPageReader
 from llama_index.llms.groq import Groq
 from llama_index.core import VectorStoreIndex, StorageContext, Document
 from pinecone import Pinecone
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.gemini import GeminiEmbedding
 from llama_index.core import Settings
+from llama_index.core.vector_stores import ExactMatchFilter, MetadataFilter
 
 load_dotenv()
 
-llm = Groq(api_key=os.getenv("GROQ_API_KEY"), model="llama-3.3-70b-versatile")
-embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-Settings.embed_model = embed_model
+
+def intialize_config():
+    """
+    loading and initializing all the global variables
+    """
+    global llm, embed_model, pc, index_name
+
+    llm = Groq(api_key=os.getenv("GROQ_API_KEY"), model="llama-3.3-70b-versatile")
+
+    embed_model = GeminiEmbedding(
+        model_name="models/text-embedding-004", api_key=os.getenv("GOOGLE_API_KEY")
+    )
+    Settings.embed_model = embed_model
+
+    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+    index_name = "scrap-embedding"
 
 
-class VectorDataBase:
-    def __init__(self):
-        self.pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-        self.index = "scrap-embedding"
+def clean_data(text: str) -> str:
+    """
+    Cleaning the scraped text to simpler, semantically meaning text removing punctuations,
+    stop words, extra spaces and links
+    """
+    stop_words = stopwords.words("english")
 
-    def _clean_data(text: str) -> str:
+    text_lwr = text.strip().lower()
+    remove_stpwrd_list = [word for word in text_lwr.split() if word not in stop_words]
+    remv_stpwrd = " ".join(remove_stpwrd_list)
 
-        stop_words = stopwords.words("english")
+    pattern = (
+        r'(\*\s*)?(\[\s*!\[([^\]]+)\]\([^)]+\s*"[^\"]*"\)\]|\[([^\]]+)\]\([^)]+\))'
+    )
+    remv_links = re.sub(pattern, "", remv_stpwrd)
 
-        text_lwr = text.strip().lower()
-        remove_stpwrd_list = [
-            word for word in text_lwr.split() if word not in stop_words
+    return remv_links
+
+
+def create_record(url: str, userId: str, namespace: str):
+    """
+    Create a record in pinecone vector store
+    steps
+    1) scrape the website
+    2) clean the text
+    3) store the cleaned text in vector database
+    """
+    try:
+        document = SimpleWebPageReader(html_to_text=True).load_data([url])
+        clean_text = clean_data(document[0].text)
+
+        doc_id = f"{userId}#{uuid.uuid4()}"
+        metadata = {"url": url, "userId": userId}
+        upsert_document = [Document(id_=doc_id, text=clean_text, metadata=metadata)]
+
+        vector_store = PineconeVectorStore(namespace=namespace, index_name=index_name)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        index = VectorStoreIndex(
+            [], embed_model=embed_model, storage_context=storage_context
+        )
+        index.insert(document=upsert_document[0])
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def load_query_engine(namespace: str, url: str, userId: str):
+    metadata_filter = MetadataFilter(
+        filters=[
+            ExactMatchFilter(key="userId", value=userId),
+            ExactMatchFilter(key="url", value=url),
         ]
-        remv_stpwrd = " ".join(remove_stpwrd_list)
+    )
+    vector_store = PineconeVectorStore(namespace=namespace, index_name=index_name)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=vector_store, embed_model=embed_model
+    )
+    query_engine = index.as_query_engine(filters=metadata_filter, llm=llm)
+    return query_engine
 
-        pattern = (
-            r'(\*\s*)?(\[\s*!\[([^\]]+)\]\([^)]+\s*"[^\"]*"\)\]|\[([^\]]+)\]\([^)]+\))'
-        )
-        remv_links = re.sub(pattern, "", remv_stpwrd)
 
-        return remv_links
+def stream_response(query: str, namespace: str, url: str, userId: str):
+    query_engine = load_query_engine(namespace=namespace, url=url, userId=userId)
+    response = query_engine.query(query)
+    return response
 
-    def _load_chat_engine(self, namespace):
-        vector_store = PineconeVectorStore(namespace=namespace, index_name=self.index)
-        index = VectorStoreIndex.from_vector_store(
-            vector_store=vector_store, embed_model=embed_model
-        )
-        chat_engine = index.as_chat_engine(chat_mode="react", llm=llm)
-        return chat_engine
 
-    def stream_response(self, query, namespace):
-        chat_engine = self._load_chat_engine(namespace=namespace)
-        response = chat_engine.stream_chat(query)
-        for token in response.response_gen:
-            print(token, end="")
-
-    def create_record(self, url: str, userId: str, namespace: str):
-
-        try:
-            document = SimpleWebPageReader(html_to_text=True).load_data([url])
-            clean_text = self._clean_data(document[0].text)
-
-            doc_id = f"{userId}#{uuid.uuid4()}"
-            metadata = {"url": url, "userId": userId}
-            upsert_document = [Document(id_=doc_id, text=clean_text, metadata=metadata)]
-
-            vector_store = PineconeVectorStore(
-                namespace=namespace, index_name=self.index
-            )
-            storage_context = StorageContext.from_defaults(vector_store=vector_store)
-            index = VectorStoreIndex(
-                [], embed_model=embed_model, storage_context=storage_context
-            )
-            index.insert(document=upsert_document[0])
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-    def delete_record(self, url: str, namespace: str):
-        pass
-
-    def update_record(self, recordId: str, namespace: str):
-        pass
+if __name__ == "__main__":
+    intialize_config()
